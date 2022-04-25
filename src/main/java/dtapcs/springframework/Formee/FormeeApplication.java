7c15a7ee1e15eb3abc01869b198bf9f3997219e7
Phạm Trần Hiền Dung <pthdung2903@gmail.com>
2022-04-25 16:05:31 +0700

    Initial BE project

---
+++ src/main/java/dtapcs/springframework/Formee/FormeeApplication.java
@@ -0,0 +1,13 @@
+package dtapcs.springframework.Formee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dtapcs/springframework/Formee/FormeeApplicationTests.java
@@ -0,0 +1,13 @@
+package dtapcs.springframework.Formee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
